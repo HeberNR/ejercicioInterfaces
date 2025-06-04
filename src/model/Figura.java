1c893335d0bd2745a228824ea05657c8879707e5
ISP63-4 <heber96nr@gmail.com>
2025-06-04 20:25:36 -0300

    a

---
+++ src/model/Figura.java
@@ -0,0 +1,12 @@
+package model;
+
+public interface Figura {
+
+  private static double calcularArea() {
+    // aca va la formula para calcular el area..
+  }
+
+  private static double calcularPerimetro() {
+    // aca va la formula para calcular el perimetro..
+  }
+}
